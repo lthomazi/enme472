b143209ff1c6b432828833743b89b6e50a4bd578
Lucas Thomazi <lthomazi00@gmail.com>
2023-05-02 12:02:19 -0400

    adds changes function for measuring distance using the ultrasonic sensor

--- main.py
+++ main.py
@@ -39,44 +39,36 @@ def main():
         #turn off water
         pass
 
-# read distance from ultrasonic
-def read_distance(sensor_number):
-    if sensor_number == 1:
-        ultrasonic_1_trigger.low()
-        utime.sleep(2)
-        ultrasonic_1_trigger.high()
-        utime.sleep(5)
-        ultrasonic_1_trigger.low()
-       
-        while ultrasonic_1_echo.value() == 0:
-            signaloff1 = utime.ticks_us()
-        
-        while echo.value() == 1:
-            signalon1 = utime.ticks_us()
-        
-        timepassed1 = signalon1 - signaloff1
-        distance1 = (timepassed1 * 0.0343) / 2
-        print("Sensor 1: ", distance1, " cm")
-
-    elif sensor_number == 2:
-        ultrasonic_2_trigger.low()
-        utime.sleep(2)
-        ultrasonic_2_trigger.high()
-        utime.sleep(5)
-        ultrasonic_2_trigger.low()
-       
-        while ultrasonic_2_echo.value() == 0:
-            signaloff2 = utime.ticks_us()
-        
-        while echo.value() == 1:
-            signalon2 = utime.ticks_us()
-        
-        timepassed2 = signalon2 - signaloff2
-        distance2 = (timepassed2 * 0.0343) / 2
-        print("Sensor 2: ", distance2, " cm")
+# Function to calculate the distance using HC-SR04
+def measure_distance(trigger_pin, echo_pin):
+    # Set up trigger pin as output
+    trigger = machine.Pin(trigger_pin, machine.Pin.OUT)
+    # Set up echo pin as input
+    echo = machine.Pin(echo_pin, machine.Pin.IN)
     
-    else:
-        raise Exception("no utrasonic sensor selected")
+    # Set trigger pin to low for 2 microseconds
+    trigger.low()
+    time.sleep_us(2)
+    
+    # Send a 10-microsecond pulse to the trigger pin
+    trigger.high()
+    time.sleep_us(10)
+    trigger.low()
+    
+    # Measure the duration of the echo pulse
+    while echo.value() == 0:
+        pass
+    start_time = time.ticks_us()
+    
+    while echo.value() == 1:
+        pass
+    end_time = time.ticks_us()
+    
+    # Calculate the distance in centimeters
+    duration = time.ticks_diff(end_time, start_time)
+    distance = (duration * 0.0343) / 2
+    
+    return distance
 
 
 # linear interpolation to map a range of values to another (from stackoverflow)
