dd1cc5365af8af8bbeec543f00df5112f53c18de
Lucas Thomazi <lthomazi00@gmail.com>
2023-05-03 19:21:30 -0400

    adds test file for motor control using the L298N

---
+++ controller_test.py
@@ -0,0 +1,26 @@
+from machine import Pin
+
+controller1 = Pin(12, Pin.OUT)
+controller2 = Pin(13, Pin.OUT)
+controller3 = Pin(14, Pin.OUT)
+controller4 = Pin(15, Pin.OUT)
+
+# Turn motor connected to OUT1 & OUT2 on in one direction
+#controller1.on()
+#controller2.off()
+
+# Turn motor connected to OUT1 & OUT2 on in other direction
+#controller1.off()
+#controller2.on()
+
+# Turn motor connected to OUT3 & OUT4 on in one direction
+#controller3.on()
+#controller4.off()
+
+# Turn motor connected to OUT3 & OUT4 on in other direction
+#controller3.off()
+#controller4.on()
+
+
+while True:
+    pass
