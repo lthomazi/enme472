0f3c28ec9e89cbaa8f93e6b317f0357a650c1bb6
manavc13 <97549664+manavc13@users.noreply.github.com>
2023-05-04 19:48:58 -0400

    adds ramp up capability

---
+++ rampUp.py
@@ -0,0 +1,21 @@
+from machine import Pin, PWM
+from time import sleep
+
+IN1 = Pin(12, Pin.OUT)
+IN2 = Pin(13, Pin.OUT)
+
+speed = PWM(Pin(10))
+speed.freq(1000)
+
+while True:
+    for i in range(65535):
+        speed.duty_u16(i)
+        IN1.low()  #spin forward
+        IN2.high()
+   
+    sleep(5)    
+    IN1.low()  #stop
+    IN2.low()
+    sleep(2)
+        
+
