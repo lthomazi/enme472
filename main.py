152d187522a4c928c1c315913e23da2b8a0447a2
Lucas Thomazi <lthomazi00@gmail.com>
2023-05-04 19:43:46 -0400

    removes ultrasonic pin definitions from funtions

--- main.py
+++ main.py
@@ -47,9 +47,6 @@ def main():
 
 # Function to calculate the distance using HC-SR04
 def measure_distance_1():
-
-    ultrasonic_1_echo = Pin(10, Pin.IN)
-    ultrasonic_1_trigger = Pin(11, Pin.OUT)
     
     # Set trigger pin to low for 2 microseconds
     ultrasonic_1_trigger.low()
@@ -76,9 +73,6 @@ def measure_distance_1():
     return distance
 
 def measure_distance_2():
-
-    ultrasonic_2_echo = Pin(10, Pin.IN)
-    ultrasonic_2_trigger = Pin(11, Pin.OUT)
     
     # Set trigger pin to low for 2 microseconds
     ultrasonic_2_trigger.low()
