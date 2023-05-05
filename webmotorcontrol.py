b08a1144185b3e4f06ce77e6ac79ed0d9e5a88bb
manavc13 <97549664+manavc13@users.noreply.github.com>
2023-05-04 21:01:51 -0400

    restores code

--- webmotorcontrol.py
+++ webmotorcontrol.py
@@ -1,201 +1,140 @@
-# This code is to test hosting a webpage with the Pico W
-import wifi
-import socket
-from machine import Pin
+''' 
+This is code for controlling our robot. The code will have to:
+    - control two motors
+    - control solenoid valves for water control
+    - measure distance of two ultrasonic sensors
+'''
 
-# led pin to test code for blink with javascript
-controller1 = Pin(12, Pin.OUT)
-controller2 = Pin(13, Pin.OUT)
-controller3 = Pin(14, Pin.OUT)
-controller4 = Pin(15, Pin.OUT)
+import machine
+from time import time, sleep
+from machine import Pin, PWM, ADC
+import _thread as thread
 
+# Ultrasonic sensor pins
+ultrasonic_1_echo = Pin(7, Pin.IN)
+ultrasonic_1_trigger = Pin(8, Pin.OUT)
+ultrasonic_2_echo = Pin(9, Pin.IN)
+ultrasonic_2_trigger = Pin(10, Pin.OUT)
 
+# motor control pins
+motor_speed1 = PWM(Pin(11))
+motor1A = Pin(12, Pin.OUT)
+motor1B = Pin(13, Pin.OUT)
+motor2A = Pin(14, Pin.OUT)
+motor2B = Pin(15, Pin.OUT)
 
-# Contents of the webpage
-def web_page():
+# Solenoid control pins
+solenoid = Pin(22, Pin.OUT)
 
-    # Define html code, with LED state passed to the server using javascript
-    html = """<html><head><title>Lean Green Cleaning Machine - Team 14</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" href="data:,">
-        <style>
-        html{font-family: Helvetica; display:inline-block; margin: 0px auto; text-align: center;}
-        h1{color: #004f1c; padding: 2vh;}
-        p{font-size: 1.5rem;}
-        .button1{display: inline-block; background-color: #e7bd3b; border: none; border-radius: 4px; color: white;
-                         padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}
-        .button2{display: inline-block; background-color: #4286f4; border: none; border-radius: 4px; color: white;
-                         padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}
-        .button2{display: inline-block; background-color:#fcba03; border: none; border-radius: 4px; color: white;
-                         padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}                 
-        </style>
-
-        <!-- 
-        <script>
-        function move_right() {
-         message sent to server
-            fetch("/move_right");
-         log message on web client
-            console.log("move right");
-         run other function
-            update_state();
-        }
-        </script>
-        -->
-
-        <script>
-        <!-- function name -->
-        function move_right() {
-            fetch("/move_right");
-            console.log("move right");
-            update_state();
-        }
-        </script>
-
-        <script>
-        function move_left() {
-            fetch("/move_left");
-            console.log("move left");
-            update_state();
-        }
-        </script>
-
-        <script>
-        function stop_motor() {
-            fetch("/stop_motor");
-            console.log("stop motor");
-            update_state();
-        }
-        </script>
-
-        <script>
-        function update_state() {
-
-            fetch("/motor_status")
-                .then(response => response.text())
-                .then(state => {
-                    document.getElementById("motor_status").innerHTML = state;
-                });
-        }
-        </script>
-
-        <!-- Web page content -->
-        </head>
-        <h1>Lean Green Cleaning Machine</h1>
-        <body onload="update_state();">
-        <p>Motor Control</p>
-        <p id="motor_status"></p>
-        <button class="button1" onclick="move_left()"> Left </button>
-        <button class="button2" onclick="move_right()"> Right </button>
-        <button class="button" onclick="stop_motor()"> STOP </button>
-        </body>
-    </html>
-    """
-    return bytes(html, 'utf-16')
-
-def move_right():
-        controller1.on() 
-        controller2.off()
-        
-
-
-def move_left():
-        controller1.off() 
-        controller2.on() 
-        motor_status = -1
-
-
-def stop_all():
-        controller1.off()
-        controller2.off()
-        controller3.off()
-        controller4.off()
-        motor_status = 0
-
-
+# main function
 def main():
-        motor_status = 0
-        # Open the text file and read its contents
-        with open("wifi_credentials.txt", "r") as f:
-                content = f.read().splitlines()
+        pass
 
-        # Extract the SSID and password from the content
-        network_name = content[0]
-        password = content[1]
-        # To access HTML page, connect to IP address via browser
-        ip = wifi.connect(network_name, password)
-        print(ip)
-        # Set up Socket
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # pass IP addr & socket type
-        # AF_INET -> IPv4 socket
-        # SOCK_STREAM -> use TCP
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.bind(('', 80))     #  # Bind HOST IP address through the given PORT 
-        # HOST can be a specific IP address, the loopback address (127.0.0.1), 
-        # or an empty string (meaning any connection will be allowed). 
-        # PORT can be a privileged port such as 80 for HTTP, or a custom port > 1023
-        s.listen(5)          # up to 5 queued connections
+def abortALL():
+    motor1_off()
+    motor2_off()
+    close_solenoid()
 
-        while True:
-            print('Waiting for connection...')
-            try:
-                conn, addr = s.accept()                 # blocking call -- code pauses until connected to client
-                print(f'Connection from {addr}')
-                
-                # Recieve data from client
-                request_data = conn.recv(1024)
-                #print(request_data)
+def motor1_forward():
+    for i in range(65535):
+        motor_speed1.duty_u16(i)
+        motor1A.low()
+        motor1B.high()
 
-                # Process request
-                if b"GET /move_right" in request_data:
-                    # Move right
-                    move_right()
-                    motor_status = 1
-                    response = "HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\nMotor moving right"
-                    conn.send(response.encode("utf-16"))
-                    conn.close()
+def motor1_reverse():
+    for i in range(65535):
+        motor_speed1.duty_u16(i)
+        motor1A.high()
+        motor1B.low()
 
-                # Process request
-                if b"GET /move_left" in request_data:
-                    # Move left
-                    move_left()
-                    motor_status = -1
-                    response = "HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\nMotor moving left"
-                    conn.send(response.encode("utf-16"))
-                    conn.close()
+def motor1_off():
+    motor1A.low()
+    motor1B.low()
 
-                # Process request
-                if b"GET /stop_motor" in request_data:
-                    # Stop Motor
-                    stop_all()
-                    motor_status = 0
-                    response = "HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\nMotor stopped"
-                    conn.send(response.encode("utf-16"))
-                    conn.close()
-                
-                # Update motor status
-                if b"GET /motor_status" in request_data:
-                    response = f"HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\n{motor_status}"
-                    conn.send(response.encode("utf-16"))
-                    conn.close()
+def motor2_forward():
+    motor2A.low()
+    motor2B.high()
 
-                # Only send the HTML page when the path is /
-                if b"GET / " in request_data:
-                    conn.send('HTTP/1.0 200 OK\n')
-                    conn.send('Content-type: text/html\n')
-                    conn.send('Connection: close\n\n')
-                    conn.sendall(web_page())
-                    conn.close()
-            
-                print(motor_status) 
+def motor2_reverse():
+    motor2A.high()
+    motor2B.low()
 
-            except OSError as e:
-                conn.close()
-                print('connection closed')
-                
+def motor2_off():
+    motor2A.low()
+    motor2B.low()
 
 
+# solenoid-relay pins
+solenoid = Pin(22,Pin.OUT)
+solenoid.value(0)
+
+# Open or close solenoid
+def open_solenoid():
+    solenoid.on()
+    time.sleep(0.5)
+
+def close_solenoid():
+    solenoid.off()
+    time.sleep(0.5)
+
+# Function to calculate the distance using ultrasonic sensor (HC-SR04)
+def measure_distance_1():
+    
+    # Set trigger pin to low for 2 microseconds
+    ultrasonic_1_trigger.low()
+    time.sleep_us(2)
+    
+    # Send a 10-microsecond pulse to the trigger pin
+    ultrasonic_1_trigger.high()
+    time.sleep_us(10)
+    ultrasonic_1_trigger.low()
+    
+    # Measure the duration of the echo pulse
+    while ultrasonic_1_echo.value() == 0:
+        pass
+    start_time = time.ticks_us()
+    
+    while ultrasonic_1_echo.value() == 1:
+        pass
+    end_time = time.ticks_us()
+    
+    # Calculate the distance in centimeters
+    duration = time.ticks_diff(end_time, start_time)
+    distance = (duration * 0.0343) / 2
+    
+    return distance
+
+def measure_distance_2():
+    
+    # Set trigger pin to low for 2 microseconds
+    ultrasonic_2_trigger.low()
+    time.sleep_us(2)
+    
+    # Send a 10-microsecond pulse to the trigger pin
+    ultrasonic_2_trigger.high()
+    time.sleep_us(10)
+    ultrasonic_2_trigger.low()
+    
+    # Measure the duration of the echo pulse
+    while ultrasonic_2_echo.value() == 0:
+        pass
+    start_time = time.ticks_us()
+    
+    while ultrasonic_2_echo.value() == 1:
+        pass
+    end_time = time.ticks_us()
+    
+    # Calculate the distance in centimeters
+    duration = time.ticks_diff(end_time, start_time)
+    distance = (duration * 0.0343) / 2
+    
+    return distance
 
 
-if __name__ == "__main__":
-        main()
+def forward():
+    pass
+
+if __name__ == '__main__':
+    main()
+    
+    
