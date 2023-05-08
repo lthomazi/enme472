26e4643b25d53aade048290f555d85b145d18ee9
Lucas Thomazi <lthomazi00@gmail.com>
2023-05-08 18:50:04 -0400

    no web page

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ This is code for controlling our robot. The code will have to:
 '''
 
 import machine
-from time import time, sleep, sleep_us, ticks_us
+from time import sleep, sleep_us, ticks_us, ticks_diff
 from machine import Pin, PWM, ADC
 import _thread as thread
 import wifi
 import socket
 
+led = Pin("LED", Pin.OUT)
+
 # Threashold to be considered a gap (in cm)
-GAP_DISTANCE = 15
+GAP_DISTANCE = 5
 
 # Ultrasonic sensor pins
 ultrasonic_1_echo = Pin(7, Pin.IN)
@@ -34,116 +36,48 @@ solenoid.value(0)
 
 # main function
 def main():
+    led.off()
     abortALL() # start the code without anything moving
-
-    # Open the text file and read its contents
-    with open("wifi_credentials.txt", "r") as f:
-            content = f.read().splitlines()
-
-    # Extract the SSID and password from the content
-    network_name = content[0]
-    password = content[1]
-    # To access HTML page, connect to IP address via browser
-    ip = wifi.connect(network_name, password)
-    print(ip)
-    # Set up Socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # pass IP addr & socket type
-    # AF_INET -> IPv4 socket
-    # SOCK_STREAM -> use TCP
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind(('', 80))     #  # Bind HOST IP address through the given PORT 
-    # HOST can be a specific IP address, the loopback address (127.0.0.1), 
-    # or an empty string (meaning any connection will be allowed). 
-    # PORT can be a privileged port such as 80 for HTTP, or a custom port > 1023
-    s.listen(5)          # up to 5 queued connections
+    measure_distance_1()
+    measure_distance_2()
+    blink_led(3)
 
     gap_count = 0 # number of gaps between panels
     over_gap = False
-    status = 0 # 0 -> stop, 1 -> run, -1 -> reverse
     # "VOID LOOP"
-    while True:
-        print('Waiting for connection...')
+    #while True:   
+    status = 0 # 0 -> stop, 1 -> run, -1 -> reverse
+
+    # forward
+    status = 1
+    motor1_forward()
+    open_solenoid()
+    motor2_on()
     
-        
-        
-        try:
-            global conn # global to be able to interupt movent on "abort" click
-            blink_led(5)
-            conn, addr = s.accept()                 # blocking call -- code pauses until connected to client
-            print(f'Connection from {addr}')
+    # stop
+    while True:
+        d1 = measure_distance_1()
+        d2 = measure_distance_2()
+        if status == 1 and d1 > GAP_DISTANCE and d2 > GAP_DISTANCE:
+            #print("OVER THE EDGE")
+            abortALL()
+            led.on()
+            # reverse
+            motor1_reverse()
+            sleep(6)
+            status = -1
             
-            # Recieve data from client
-            request_data = conn.recv(1024)
-            #print(request_data)
-
-            measure_distance_1()
-            measure_distance_2()
-
-            # RECIEVE request
-            if b"GET /run" in request_data:
-                print("Run Recieved")
-                status = 1
-                motor1_forward()
-                motor2_on() # brush on
-                open_solenoid() # open water
-                response = "HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\nRunning"
-                conn.send(response.encode("utf-16"))
-                conn.close()
-
+            
+        if status == -1 and measure_distance_2() > GAP_DISTANCE:
+            sleep(1)
+            abortALL()
                 
-
-            # RECIEVE request
-            if b"GET /abort" in request_data:
-                # Stop Motor
-                abortALL()
-                status = 0
-                response = "HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\nAborted"
-                conn.send(response.encode("utf-16"))
-                conn.close()
+                #print("END")
+        
+        sleep(0.2)
             
-            # RECIEVE request
-            if b"GET /reset" in request_data:
-                motor2_off()        # turn off brush
-                close_solenoid()    # turn water off
-                motor1_reverse()    # move motor back
-                status = -1
-                motor1_off()
-                response = "HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\nResetting"
-                conn.send(response.encode("utf-16"))
-                conn.close()
-            
-            # RECIEVE request
-            if b"GET /brush_on" in request_data:
-                motor2_on()
-                response = "HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\nBrush On"
-                conn.send(response.encode("utf-16"))
-                conn.close()
 
-            # SEND motor status
-            if b"GET /status" in request_data:
-                print(status)
-                response = f"HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\n{status}"
-                conn.send(response.encode("utf-16"))
-                conn.close()
-
-            # Only send the HTML page when the path is /
-            if b"GET / " in request_data:
-                conn.send('HTTP/1.0 200 OK\n')
-                conn.send('Content-type: text/html\n')
-                conn.send('Connection: close\n\n')
-                conn.sendall(web_page())
-                conn.close()
-    # -----------------END OF VOID LOOP-----------------------------------------------    
-
-        except OSError as e:
-            conn.close()
-            print('connection closed')
-
-# Web page content
-def web_page():
-    with open("index.html", "r", encoding="utf-8") as file:
-        html = file.read()
-    return bytes(html, 'utf-16')
+    
 
 # blink led x times
 def blink_led(x):
@@ -154,12 +88,13 @@ def blink_led(x):
         led.off()
         sleep(1)
     
-    led.on()
+
     
 
 
 # Stop moving, stop water, stop brush
 def abortALL():
+    print("ABORT ALL")
     motor1_off()
     motor2_off()
     close_solenoid()
@@ -205,16 +140,6 @@ def close_solenoid():
 
 # Function to calculate the distance using ultrasonic sensor (HC-SR04)
 def measure_distance_1():
-    # Check for if abort button is pressed while measuring distance
-    # Recieve data from client
-    request_data = conn.recv(1024)
-    # Process request
-    if b"GET /abort" in request_data:
-        # Stop Motor
-        abortALL()
-        response = "HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\nMotor stopped"
-        conn.send(response.encode("utf-16"))
-        conn.close()
 
     # Set trigger pin to low for 2 microseconds
     ultrasonic_1_trigger.low()
@@ -235,22 +160,12 @@ def measure_distance_1():
     end_time = ticks_us()
     
     # Calculate the distance in centimeters
-    duration = time.ticks_diff(end_time, start_time)
+    duration = ticks_diff(end_time, start_time)
     distance = (duration * 0.0343) / 2
-    print("Distance 1: ", distance)
+    #print("Distance 1: ", distance)
     return distance
 
 def measure_distance_2():
-    # Check for if abort button is pressed while measuring distance
-    # Recieve data from client
-    request_data = conn.recv(1024)
-    # Process request
-    if b"GET /abort" in request_data:
-        # Stop Motor
-        abortALL()
-        response = "HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\nMotor stopped"
-        conn.send(response.encode("utf-16"))
-        conn.close()
 
     # Set trigger pin to low for 2 microseconds
     ultrasonic_2_trigger.low()
@@ -271,9 +186,9 @@ def measure_distance_2():
     end_time = ticks_us()
     
     # Calculate the distance in centimeters
-    duration = time.ticks_diff(end_time, start_time)
+    duration = ticks_diff(end_time, start_time)
     distance = (duration * 0.0343) / 2
-    print("Distance 2: ", distance)
+    #print("Distance 2: ", distance)
     return distance
 
 
@@ -281,3 +196,4 @@ if __name__ == '__main__':
     main()
     
     
+
